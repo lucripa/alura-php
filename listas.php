dfa716eb5a6d3e75bab1c92121da91993842c821
lucripa <l.cripa@gmail.com>
2021-03-02 23:44:12 -0300

    adicionando arquivos de aulas anteriores

---
+++ listas.php
@@ -0,0 +1,7 @@
+<?php
+$idadeList = [21, 23, 19, 34, 32, 22, 27, 28];
+echo count($idadeList) . PHP_EOL ;
+for ($idades = 0; $idades < count($idadeList); $idades++){
+    echo $idadeList[$idades] . PHP_EOL;
+}
+
